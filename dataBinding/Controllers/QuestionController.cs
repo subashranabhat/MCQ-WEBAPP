41fe13bc7eafb03b7e075ca44d52bf041c5c258a
Subash Ranabhat <subashkumar411@gmail.com>
2017-10-21 02:20:36 -0500

    DataBindingNeedsFix

---
+++ dataBinding/Controllers/QuestionController.cs
@@ -0,0 +1,24 @@
+﻿using dataBinding.Models;
+using System.Web.Mvc;
+
+namespace dataBinding.Controllers
+{
+  public class QuestionController : Controller
+  {
+    // GET: Question
+    public ActionResult Index()
+    {
+      //List<Question> qlist = new List<Question>();
+
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Index(Question[] q)
+    {
+      //List<Question> qlist = new List<Question>();
+
+      return View();
+    }
+  }
+}
---
+++ dataBinding/Models/Question.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dataBinding.Models
+{
+    public class Question
+    {
+      private string value;
+
+    }
+}
